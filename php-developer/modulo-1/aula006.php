add652bbcb0d148ef173a639066fb2b4f0d1ff0e
nely de faria <nelly.nf54@gmail.com>
2021-03-17 19:19:18 -0300

    Adiciona arquivo aula006.php

---
+++ php-developer/modulo-1/aula006.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Incremento e Decremento</title>
+    </head>
+<body>
+          
+        <?php
+
+        echo "<h3>Pós-incremento</h3>";
+        $a = 5;
+        echo "Deve ser 5: " . $a++ . "<br>";
+        echo "Deve ser 6: " . $a . "<br>";
+
+        echo "<h3>Pré-incremento</h3>";
+        $a = 5;
+        echo "Deve ser 6: " . ++$a . "<br>";
+        echo "Deve ser 6: " . $a . "<br>";
+
+        echo "<h3>Pós-decremento</h3>";
+        $a = 5;
+        echo "Deve ser 5: " . $a-- . "<br>";
+        echo "Deve ser 4: " . $a . "<br>";
+
+        echo "<h3>Pré-decremento</h3>";
+        $a = 5;
+        echo "Deve ser 4: " . --$a . "<br>";
+        echo "Deve ser 4: " . $a . "<br>";
+
+        ?>
+        
+</body>
+</html>
