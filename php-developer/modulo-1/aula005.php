4039c7b4a247719de0fdabcfbbd05b5bd1753a67
nely de faria <nelly.nf54@gmail.com>
2021-03-17 19:18:42 -0300

    Adiciona arquivo aula005.php

---
+++ php-developer/modulo-1/aula005.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 1;
+$b = 2;
+$c = 3;
+$result = 6;
+
+$result += $a;
+echo "Resultado da adição: " . $result . "<br>";
+
+$result -= $b;
+echo "Resultado da subtração: " . $result . "<br>";
+
+$result *= $b;
+echo "Resultado da multiplicação: " . $result . "<br>";
+
+$result /= $b;
+echo "Resultado da divisão: " . $result . "<br>";
+
+$result %= $b;
+echo "Resultado da modulo: " . $result . "<br>";
+
+$d = "Bom ";
+$d .= "Dia";
+echo $d;
+
+
+
