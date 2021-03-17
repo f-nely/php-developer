9d2461005fbe5de1bd2968f58e109d250311fb13
nely de faria <nelly.nf54@gmail.com>
2021-03-17 20:41:14 -0300

    Adiciona arquivo aula010.php

---
+++ php-developer/modulo-1/aula010.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Estrutura de Controle</title>
+</head>
+<body>
+    <?php 
+      $a = 3;
+
+      if ($a == 2) {
+        echo "Sacar Dinheiro";
+      } else {
+        echo "Opção não encontrada";
+      }
+    ?>
+</body>
+</html>
