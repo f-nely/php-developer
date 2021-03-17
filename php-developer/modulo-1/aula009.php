9baf3e8bf8cd7634a0b987e215015f27430d62f5
nely de faria <nelly.nf54@gmail.com>
2021-03-17 20:36:29 -0300

    Adiciona arquivo aula009.php

---
+++ php-developer/modulo-1/aula009.php
@@ -0,0 +1,19 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Estrutura de Controle</title>
+</head>
+<body>
+    <?php 
+        $a = 2;
+        if ($a == 1) {
+            echo "Sacar Dinheiro";
+        }
+
+        if ($a == 2) {
+            echo "Depositar Dinheiro";
+        }
+    ?>
+</body>
+</html>
