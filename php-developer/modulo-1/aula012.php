db5ef81894160cfa93622735718189b09b03faf6
nely de faria <nelly.nf54@gmail.com>
2021-03-17 21:26:49 -0300

    Adiciona arquivo aula012.php

---
+++ php-developer/modulo-1/aula012.php
@@ -0,0 +1,27 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Switch</title>
+</head>
+<body>
+    <?php 
+        $a = 3;
+
+        switch($a) {
+            case 1:
+                echo "Sacar Dinheiro";
+                break;
+            case 2:
+                echo "Depositar Dinheiro";
+                break;
+            case 3:
+                echo "Imprimir Cheque";    
+                break;
+            default:
+                echo "Opção não encontrada";
+                break;    
+        }
+    ?>
+</body>
+</html>
