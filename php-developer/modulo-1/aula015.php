7cec6f9b4724568fe29643371c4ea0f28ab0af11
nely de faria <nelly.nf54@gmail.com>
2021-03-17 21:44:43 -0300

    Adiciona arquivo aula015.php

---
+++ php-developer/modulo-1/aula015.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>For</title>
+</head>
+<body>
+    <?php 
+       for ($i = 1; $i <= 10; $i++) {
+            echo "E-mail enviado: $i <br>";
+       }
+    ?>
+</body>
+</html>
