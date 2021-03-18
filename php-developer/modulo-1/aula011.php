b0e96bfa39f45299c72b82d584f8525c3914bc96
nely de faria <nelly.nf54@gmail.com>
2021-03-17 21:19:28 -0300

    Adiciona arquivo aula011.php

---
+++ php-developer/modulo-1/aula011.php
@@ -0,0 +1,19 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>IF ... eleseif ... else</title>
+</head>
+<body>
+    <?php 
+      $a = 2;
+      if ($a == 2) {
+          echo "Depositar dinheiro";
+      } elseif ($a == 1) {
+          echo "Sacar dinheiro";
+      } else {
+          echo "Opção não encontrada";
+      }
+    ?>
+</body>
+</html>
