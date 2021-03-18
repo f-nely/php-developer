69a914fcad98a09492aa6e03481b84307150a5d6
nely de faria <nelly.nf54@gmail.com>
2021-03-17 21:32:14 -0300

    Adiciona arquivo aula013.php

---
+++ php-developer/modulo-1/aula013.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>While</title>
+</head>
+<body>
+    <?php 
+        $a = 1;
+
+        while($a <= 10) {
+            echo "E-mail enviado: $a <br>";
+            $a++;
+        }
+
+    ?>
+</body>
+</html>
