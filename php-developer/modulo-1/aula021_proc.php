5f184e365fd332a3335e9edfe377d9db1cb7d410
nely de faria <nelly.nf54@gmail.com>
2021-03-18 17:43:09 -0300

    Adiciona arquivo aula021_proc.php

---
+++ php-developer/modulo-1/aula021_proc.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome_cliente = $_POST['nome_cliente'];
+$email_cliente = $_POST['email_cliente'];
+
+echo "Nome do cliente: $nome_cliente <br>";
+echo "E-mail do cliente: $email_cliente <br>";
+
