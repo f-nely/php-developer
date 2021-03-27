3d6a4fc68a3ce7733f66895ecf2882c8e6dd8307
nely de faria <nelly.nf54@gmail.com>
2021-03-27 20:11:47 -0300

    Imprimindo o resultado da query

--- php-developer/modulo-2/aula004-limit.php
+++ php-developer/modulo-2/aula004-limit.php
@@ -8,3 +8,21 @@ $dbname = 'celke';
 
 // cria conexão
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+/* $result_usuario = "SELECT id, nome, email FROM usuarios LIMIT 2";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+    echo "ID: " . $row_usuario['id'] . "<br>";
+    echo "Nome: " . $row_usuario['nome'] . "<br>";
+    echo "Email: " . $row_usuario['email'] . "<br><hr>";
+} */
+
+$result_usuario = "SELECT id, nome, email FROM usuarios LIMIT 2 OFFSET 2";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+    echo "ID: " . $row_usuario['id'] . "<br>";
+    echo "Nome: " . $row_usuario['nome'] . "<br>";
+    echo "Email: " . $row_usuario['email'] . "<br><hr>";
+}
