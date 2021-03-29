7117aafe3e50005260b4f1d6ce4faebb0b64a34b
nely de faria <nelly.nf54@gmail.com>
2021-03-29 18:56:31 -0300

    Utilizando cláusula inner join

--- php-developer/modulo-2/aula015-inner-join.php
+++ php-developer/modulo-2/aula015-inner-join.php
@@ -7,12 +7,15 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$result_usuario = "SELECT user.id, user.email, user.niveis_acesso_id 
-FROM usuarios AS user";
+$result_usuario = "SELECT user.id, user.email, user.niveis_acesso_id,
+nivac.nome 
+FROM usuarios AS user
+INNER JOIN niveis_acessos AS nivac ON nivac.id = user.niveis_acesso_id";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
     echo "ID: " . $row_usuario['id'] . "<br>";
     echo "E-mail: " . $row_usuario['email'] . "<br>";
+    echo "Nome nível acesso: " . $row_usuario['nome'] . "<br>";
     echo "Níveis de acesso id: " . $row_usuario['niveis_acesso_id'] . "<br><hr>";
 }
