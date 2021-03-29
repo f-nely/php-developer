3fd4c9cbba54483da4b04fbd66251e0981fc617d
nely de faria <nelly.nf54@gmail.com>
2021-03-29 12:03:58 -0300

    Adiciona arquivo aula009-update.php

---
+++ php-developer/modulo-2/aula009-update.php
@@ -0,0 +1,18 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '704802';
+$dbname = 'celke';
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$result_usuario = "UPDATE usuarios SET situacao_id = '1', modified = NOW() WHERE id = '5'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if (mysqli_affected_rows($conn)) {
+    echo "Alterado com sucesso <br>";
+    echo mysqli_affected_rows($conn);
+} else {
+    echo "Erro ao alterar o usuário";
+}
