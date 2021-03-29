1fb69f3d507e9e890b3756d225de29312eda8020
nely de faria <nelly.nf54@gmail.com>
2021-03-29 12:29:39 -0300

    Pesquisando resultado com like

--- php-developer/modulo-2/aula011-like.php
+++ php-developer/modulo-2/aula011-like.php
@@ -7,4 +7,11 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
+$result_usuario = "SELECT * FROM usuarios WHERE nome LIKE '%a%'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
 
+while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+    echo "ID: " . $row_usuario['id'] . "<br>";
+    echo "Nome: " . $row_usuario['nome'] . "<br>";
+    echo "Email: " . $row_usuario['email'] . "<br><hr>";
+}
