111f5d85c57c08225d95544cd8f2a14ca64fb107
nely de faria <nelly.nf54@gmail.com>
2021-03-28 22:49:23 -0300

    Inserindo dados no banco

--- php-developer/modulo-2/aula008-insert-into.php
+++ php-developer/modulo-2/aula008-insert-into.php
@@ -8,3 +8,14 @@ $dbname = 'celke';
 
 // cria conexão
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$result_usuario = "INSERT INTO usuarios 
+(nome, email, situacao_id, niveis_acesso_id, created)
+VALUES
+('Carlos', 'carlos@celke.com.br', '2', '3', NOW())";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if (mysqli_insert_id($conn)) {
+    echo "Inserido com sucesso!<br>";
+    echo "ID: " . mysqli_insert_id($conn);
+}
