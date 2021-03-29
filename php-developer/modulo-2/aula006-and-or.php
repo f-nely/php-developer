cdff3232e21645631ac70a69149c357d3214e8ec
nely de faria <nelly.nf54@gmail.com>
2021-03-28 22:20:20 -0300

    Adiciona arquivo aula006-and-or.php

---
+++ php-developer/modulo-2/aula006-and-or.php
@@ -0,0 +1,20 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '704802';
+$dbname = 'celke';
+
+
+// cria conexão
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+$result_usuario = "SELECT * FROM usuarios WHERE situacao_id = '1' AND niveis_acesso_id = '1'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+    echo "ID: " . $row_usuario['id'] . "<br>";
+    echo "Nome: " . $row_usuario['nome'] . "<br>";
+    echo "Email: " . $row_usuario['email'] . "<br>";
+    echo "Situação id: " . $row_usuario['situacao_id'] . "<br>";
+    echo "Niveis acesso id: " . $row_usuario['niveis_acesso_id'] . "<br><hr>";
+}
