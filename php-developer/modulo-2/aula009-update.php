f7b4bc5cf60decec767b37348cfceada826d2dcf
nely de faria <nelly.nf54@gmail.com>
2021-03-29 12:07:39 -0300

    Alterando query

--- php-developer/modulo-2/aula009-update.php
+++ php-developer/modulo-2/aula009-update.php
@@ -7,7 +7,7 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$result_usuario = "UPDATE usuarios SET situacao_id = '1', modified = NOW() WHERE id = '5'";
+$result_usuario = "UPDATE usuarios SET situacao_id = '1', niveis_acesso_id = '2', modified = NOW() WHERE id = '5'";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if (mysqli_affected_rows($conn)) {
