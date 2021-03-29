6da18b19fae88dcfd4980e1f956bd29519690510
nely de faria <nelly.nf54@gmail.com>
2021-03-29 19:02:59 -0300

    Modificando query

--- php-developer/modulo-2/aula015-inner-join.php
+++ php-developer/modulo-2/aula015-inner-join.php
@@ -7,15 +7,16 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$result_usuario = "SELECT user.id, user.email, user.niveis_acesso_id,
-nivac.nome 
+$result_usuario = "SELECT user.id, user.nome nome_usuario, user.email, user.niveis_acesso_id,
+nivac.nome  nome_niv_ac
 FROM usuarios AS user
 INNER JOIN niveis_acessos AS nivac ON nivac.id = user.niveis_acesso_id";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
     echo "ID: " . $row_usuario['id'] . "<br>";
+    echo "Nome usuário: " . $row_usuario['nome_usuario'] . "<br>";
     echo "E-mail: " . $row_usuario['email'] . "<br>";
-    echo "Nome nível acesso: " . $row_usuario['nome'] . "<br>";
+    echo "Nome nível acesso: " . $row_usuario['nome_niv_ac'] . "<br>";
     echo "Níveis de acesso id: " . $row_usuario['niveis_acesso_id'] . "<br><hr>";
 }
