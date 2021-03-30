55f1f6bff5f4977eda90dd2acdac2c4c3cb39634
nely de faria <nelly.nf54@gmail.com>
2021-03-30 18:48:16 -0300

    Usando cláusula where

--- php-developer/modulo-2/aula020-sum.php
+++ php-developer/modulo-2/aula020-sum.php
@@ -7,7 +7,7 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$result_usuarios = "SELECT SUM(qnt_acessos) AS qtn_acessos FROM usuarios";
+$result_usuarios = "SELECT SUM(qnt_acessos) AS qtn_acessos FROM usuarios WHERE situacao_id = 1";
 $resultado_usuarios = mysqli_query($conn, $result_usuarios);
 $row_usuarios = mysqli_fetch_assoc($resultado_usuarios);
 
