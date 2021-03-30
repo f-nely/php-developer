3cc323ae4545a260c9ed00f9cdf1dc818da9eb88
nely de faria <nelly.nf54@gmail.com>
2021-03-30 18:40:40 -0300

    Adiciona arquivo aula020-sum.php

---
+++ php-developer/modulo-2/aula020-sum.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '704802';
+$dbname = 'celke';
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$result_usuarios = "SELECT SUM(qnt_acessos) AS qtn_acessos FROM usuarios";
+$resultado_usuarios = mysqli_query($conn, $result_usuarios);
+$row_usuarios = mysqli_fetch_assoc($resultado_usuarios);
+
+echo "Total de acesso ao site: " . $row_usuarios['qtn_acessos'] . "<br>";
