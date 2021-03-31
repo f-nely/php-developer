d66ca9da24c908b9c2f3c9583225a692e9fbe7cd
nely de faria <nelly.nf54@gmail.com>
2021-03-31 13:05:51 -0300

    Criando formulário

--- php-developer/modulo-2/aula022-crud-inserir.php
+++ php-developer/modulo-2/aula022-crud-inserir.php
@@ -6,6 +6,17 @@
 </head>
 <body>
     <h1>Cadastrar Usuário</h1>
+
+    <form method="POST" action="aula023-crud-proc.php">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome"><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"><br><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+
 </body>
 </html>
 
