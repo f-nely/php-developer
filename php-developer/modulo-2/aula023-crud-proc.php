89f10ba26b45b16cab59035fb5cb1e33841c16ba
nely de faria <nelly.nf54@gmail.com>
2021-03-31 13:18:06 -0300

    Criando query

--- php-developer/modulo-2/aula023-crud-proc.php
+++ php-developer/modulo-2/aula023-crud-proc.php
@@ -7,3 +7,12 @@ $dbname = 'celke';
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+$result_usuarios = "INSERT INTO usuarios 
+(nome, email, situacao_id, niveis_acesso_id, created)
+VALUES
+('$nome', '$email', 2, 3, NOW())";
+
+$resultado_usuarios = mysqli_query($conn, $result_usuarios);
