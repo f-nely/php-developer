afc2ab1c7a114eb8fc4abdfb51f2ba5378591e05
nely de faria <nelly.nf54@gmail.com>
2021-04-01 17:40:50 -0300

    Criando query, buscando dados do banco e mensagem

--- php-developer/modulo-2/aula025-crud-proc.php
+++ php-developer/modulo-2/aula025-crud-proc.php
@@ -4,3 +4,22 @@ session_start();
 
 include_once('conexao.php');
 
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+$result_usuario = "UPDATE usuarios SET 
+    nome = '$nome',
+    email = '$email',
+    modified = NOW()
+    WHERE id = '$id'";
+
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if (mysqli_affected_rows($conn)) {
+    $_SESSION['msg'] = "<span style='color: rgb(0, 255,0);'>Usuário editado com sucesso</span>";
+    header("Location: aula021-crud-listar.php");
+} else {
+    $_SESSION['msg'] = "<span style='color: rgb(255, 0,0);'>Usuário não foi editado com sucesso</span>";
+    header("Location: aula024-crud-editar.php");
+}
