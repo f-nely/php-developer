706215698e45ad2c299563ec27241af1441612ea
nely de faria <nelly.nf54@gmail.com>
2021-04-01 17:03:48 -0300

    Criando formulário

--- php-developer/modulo-2/aula024-crud-editar.php
+++ php-developer/modulo-2/aula024-crud-editar.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 include_once('conexao.php');
 $id  = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$result_usuarios = "SELECT * FROM usuarios WHERE id = '$id' LIMIT";
-$resultado_usuarios = mysqli_query($conn, $result_usuarios);
-$row_usuario = mysqli_fetch_assoc($resultado_usuarios);
+$result_usuario = "SELECT * FROM usuarios WHERE id = '$id' LIMIT 1";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+$row_usuario = mysqli_fetch_assoc($resultado_usuario);
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +16,24 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuarios);
     <title>Crud - Cadastrar</title>
 </head>
 <body>
-    
+
+    <a href="aula021-crud-listar.php">Listar</a>
+    <h1>Editar Usuário</h1>
+    <?php
+        if (isset($_SESSION['msg'])) {
+            echo "<p>" . $_SESSION['msg'] . "</p>";
+            unset($_SESSION['msg']);
+        }
+    ?>
+    <form method="POST" action="aula025-crud-proc.php">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome"><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"><br><br>
+
+        <input type="submit" value="Editar">
+    </form>
+
 </body>
 </html>
