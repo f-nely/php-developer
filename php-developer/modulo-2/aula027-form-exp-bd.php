e01ea9c58420dcc68de82fba775c6768e2f65908
nely de faria <nelly.nf54@gmail.com>
2021-04-02 11:14:21 -0300

    Criando formulário

--- php-developer/modulo-2/aula027-form-exp-bd.php
+++ php-developer/modulo-2/aula027-form-exp-bd.php
@@ -7,6 +7,26 @@
     <title>Exportar DB</title>
 </head>
 <body>
-    
+
+    <h1>Exportar base de dados</h1>
+
+    <form method="POST" action="aula028-proc-exp-bd.php" enctype="multipart/form-data">
+
+        <label for="servidor">Servidor:</label>
+        <input type="text" name="servidor" id ="servidor" placeholder="Nome do Servidor"><br><br>
+
+        <label for="usuario">Usuário:</label>
+        <input type="text" name="usuario" id="usuario" placeholder="Nome do Usuário"><br><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha" placeholder="Senha da base de dados"><br><br>
+
+        <label for="dbname">Base de Dados:</label>
+        <input type="text" name="dbname" id="dbname" placeholder="Nome da base de dados"><br><br>
+        
+        <input type="submit" value="Exportar">
+        
+    </form>
+
 </body>
 </html>
