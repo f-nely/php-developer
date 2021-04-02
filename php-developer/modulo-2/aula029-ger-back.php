b803cbc5231f9910991c1063dbb600cc4a3ea244
nely de faria <nelly.nf54@gmail.com>
2021-04-02 11:44:14 -0300

    Pesquisando tabelas e quantidade de colunas

--- php-developer/modulo-2/aula029-ger-back.php
+++ php-developer/modulo-2/aula029-ger-back.php
@@ -7,7 +7,16 @@ $result_tabela = "SHOW TABLES";
 $resultado_tabela = mysqli_query($conn, $result_tabela);
 
 while ($row_tabela = mysqli_fetch_row($resultado_tabela)) {
-    $tabela[] = $row_tabela[0];
+    $tabelas[] = $row_tabela[0];
 }
 
-var_dump($tabela);
+//var_dump($tabelas);
+
+foreach($tabelas as $tabela) {
+    //echo $tabela . '<br>';
+    //pesquisar o nome das colunas
+    $result_colunas = "SELECT * FROM ".$tabela;
+    $resultado_colunas = mysqli_query($conn, $result_colunas);
+    $num_colunas = mysqli_num_fields($resultado_colunas);
+    //echo $tabela . " - " . $num_colunas . "<br>";
+}
