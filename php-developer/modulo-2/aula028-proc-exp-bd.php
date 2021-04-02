68df69db1a48bf5153a890386f4709c6d876f6d6
nely de faria <nelly.nf54@gmail.com>
2021-04-02 11:23:34 -0300

    Criando conexão com banco de dados

--- php-developer/modulo-2/aula028-proc-exp-bd.php
+++ php-developer/modulo-2/aula028-proc-exp-bd.php
@@ -5,3 +5,6 @@ $servidor = filter_input(INPUT_POST, 'servidor', FILTER_SANITIZE_STRING);
 $usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
 $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 $dbname = filter_input(INPUT_POST, 'dbname', FILTER_SANITIZE_STRING);
+
+// criar a conexão com banco de dados
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
