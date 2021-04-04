70e21bd256b808eec7d8ff009ccb0d3ef699cc1f
nely de faria <nelly.nf54@gmail.com>
2021-04-04 17:33:33 -0300

    Criando sets

--- php-developer/modulo-3/aula4_clone/Nota.php
+++ php-developer/modulo-3/aula4_clone/Nota.php
@@ -20,4 +20,30 @@ class Nota
     {
         return "Nome do aluno: {$this->aluno} <br> Displina: {$this->disciplina} <br> Nota: " . ($this->prova + $this->trabalho) . "<br><hr>";
     }
+
+    /**
+     * @param mixed $prova
+     */
+    public function setProva($prova)
+    {
+        $this->prova = $prova;
+    }
+
+    /**
+     * @param mixed $trabalho
+     */
+    public function setTrabalho($trabalho)
+    {
+        $this->trabalho = $trabalho;
+    }
+
+    /**
+     * @param mixed $disciplina
+     */
+    public function setDisciplina($disciplina)
+    {
+        $this->disciplina = $disciplina;
+    }
+
+
 }
