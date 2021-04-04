d723a9ec6875c06a78fc11d14fb9d989a761fd51
nely de faria <nelly.nf54@gmail.com>
2021-04-04 17:33:52 -0300

    Usando função clone

--- php-developer/modulo-3/aula4_clone/aula04.php
+++ php-developer/modulo-3/aula4_clone/aula04.php
@@ -11,6 +11,13 @@
 
         $nota->setNota('Cesar Szpak', 'Matemática', 5, 2);
         echo $nota->getNota();
+
+        $nota2 = clone $nota;
+        $nota2->setDisciplina('História');
+        $nota2->setProva(6);
+        $nota2->setTrabalho(4);
+
+        echo $nota2->getNota();
     ?>
 </body>
 </html>
