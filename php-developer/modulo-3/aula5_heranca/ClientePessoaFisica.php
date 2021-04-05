0e3205b004d5e9cb39e713a9e0226aded5685110
nely de faria <nelly.nf54@gmail.com>
2021-04-05 17:33:37 -0300

    Adiciona classe ClientePessoaFisica

---
+++ php-developer/modulo-3/aula5_heranca/ClientePessoaFisica.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class ClientePessoaFisica
+{
+    public $nome;
+    public $cpf;
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+}
