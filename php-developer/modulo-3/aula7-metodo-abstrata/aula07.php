27a3cbed8ddd578fefc429a7f986ea0ed0f49bc9
nely de faria <nelly.nf54@gmail.com>
2021-04-08 18:52:06 -0300

    Adiciona arquivo aula07.php

---
+++ php-developer/modulo-3/aula7-metodo-abstrata/aula07.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Celke</title>
+</head>
+<body>
+    <?php
+        require_once 'Cheque.php';
+        require_once 'ChequeComum.php';
+        require_once 'ChequeEspecial.php';
+
+        /*$cheque = new Cheque(100.10, 'comum');
+        echo $cheque->verValor();*/
+
+        $chequeComum = new ChequeComum(100.10, 'comum');
+        echo $chequeComum->calcularJuros();
+
+        $chequeEspecial = new ChequeEspecial(100.10, 'especial');
+        echo $chequeEspecial->calcularJuros();
+    ?>
+</body>
+</html>
+
+
+
