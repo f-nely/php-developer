15fef60b3e82f2e35e8de75a5082030286b43e40
nely de faria <nelly.nf54@gmail.com>
2021-04-08 18:49:58 -0300

    Adiciona arquivo Cheque.php

---
+++ php-developer/modulo-3/aula7-metodo-abstrata/Cheque.php
@@ -0,0 +1,26 @@
+<?php
+
+
+abstract class Cheque
+{
+    public $valor;
+    public $tipo;
+
+    public function __construct($valor, $tipo)
+    {
+        $this->valor = $valor;
+        $this->tipo = $tipo;
+    }
+
+    public function verValor()
+    {
+        return "Valor do cheque {$this->tipo}: R$ {$this->real($this->valor)} <hr>";
+    }
+
+    public function real($valor)
+    {
+        return number_format($valor, '2', ',', '.');
+    }
+
+    abstract function calcularJuros();
+}
