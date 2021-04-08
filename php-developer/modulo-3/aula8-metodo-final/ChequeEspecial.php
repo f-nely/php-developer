9417ddbb2af386bebe3331db1b36bbbbb78836e6
nely de faria <nelly.nf54@gmail.com>
2021-04-08 19:06:08 -0300

    Adiciona classe ChequeEspecial

---
+++ php-developer/modulo-3/aula8-metodo-final/ChequeEspecial.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class ChequeEspecial extends Cheque
+{
+    public function calcularJuros()
+    {
+        $this->valor = $this->valor * 1.10;
+        $this->valor = parent::real($this->valor);
+        return "Valor do cheque {$this->tipo}: R$ {$this->valor} <hr>";
+    }
+}
